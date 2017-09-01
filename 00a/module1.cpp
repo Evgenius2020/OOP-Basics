a88167d420ba0ec315f42af6217dae558ef104a7
Заварзин Евгений Андреевич <  >
2017-09-01 09:53:14 +0700

    Completed

---
+++ 00a/module1.cpp
@@ -0,0 +1,12 @@
+#include "module1.h"
+
+using namespace std;
+
+namespace Module1
+{
+	string getMyName()
+	{
+		string name = "John";
+		return name;
+	}
+}
---
+++ 00a/module2.cpp
@@ -0,0 +1,12 @@
+#include "module2.h"
+
+using namespace std;
+
+namespace Module2
+{
+	string getMyName()
+	{
+		string name = "James";
+		return name;
+	}
+}
