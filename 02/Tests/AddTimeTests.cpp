edad118df2138af91d9105b2f73a81ef1bde2091
Evgenius2020 <spleen47@mail.ru>
2017-09-25 10:07:16 +0700

    02 Refactoring, gtest files reduction

--- 02/Tests/AddTimeTests.cpp
+++ 02/Tests/AddTimeTests.cpp
@@ -1,65 +1,7 @@
-#include "src\gtest-all.cc"
-#include "Date.h"
+#include "gtest.h"
+#include "../Source/Date.h"
 
 using namespace DateTools;
-
-TEST(ConstructorsTests, CopyConstructors) {
-	Date date = Date(2017, Month::Jan, 1, 0, 0, 0);
-	ASSERT_EQ(2017, date.getYear());
-	ASSERT_EQ(Month::Jan, date.getMonth());
-	ASSERT_EQ(1, date.getDay());
-
-	Date dateSame = Date(2017, Month::Jan, 1);
-	ASSERT_EQ(date.getYear(), dateSame.getYear());
-	ASSERT_EQ(date.getMonth(), dateSame.getMonth());
-	ASSERT_EQ(date.getDay(), dateSame.getDay());
-	ASSERT_EQ(date.getHours(), dateSame.getHours());
-	ASSERT_EQ(date.getMinutes(), dateSame.getMinutes());
-	ASSERT_EQ(date.getSeconds(), dateSame.getSeconds());
-
-	dateSame = Date(date);
-	ASSERT_EQ(date.getYear(), dateSame.getYear());
-	ASSERT_EQ(date.getMonth(), dateSame.getMonth());
-	ASSERT_EQ(date.getDay(), dateSame.getDay());
-	ASSERT_EQ(date.getHours(), dateSame.getHours());
-	ASSERT_EQ(date.getMinutes(), dateSame.getMinutes());
-	ASSERT_EQ(date.getSeconds(), dateSame.getSeconds());
-}
-
-TEST(ConstructorsTests, CurrentTimeConstructors) {
-	Date date = Date();
-
-	time_t rawtime;
-	time(&rawtime);
-	struct tm ptm;
-	gmtime_s(&ptm, &rawtime);
-
-	ASSERT_EQ(ptm.tm_year + 1900, date.getYear());
-	ASSERT_EQ((Month)(ptm.tm_mon + 1), date.getMonth());
-	ASSERT_EQ(ptm.tm_mday, date.getDay());
-	ASSERT_EQ(ptm.tm_hour, date.getHours());
-	ASSERT_EQ(ptm.tm_min, date.getMinutes());
-	ASSERT_GE(ptm.tm_sec, date.getSeconds());
-
-	date = Date(ptm.tm_hour, ptm.tm_min, ptm.tm_sec);
-	ASSERT_EQ(ptm.tm_year + 1900, date.getYear());
-	ASSERT_EQ((Month)(ptm.tm_mon + 1), date.getMonth());
-	ASSERT_EQ(ptm.tm_mday, date.getDay());
-	ASSERT_EQ(ptm.tm_hour, date.getHours());
-	ASSERT_EQ(ptm.tm_min, date.getMinutes());
-	ASSERT_EQ(ptm.tm_sec, date.getSeconds());
-}
-
-TEST(ConstructorsTests, WrongInitializing) {
-	Date date = Date(2010, Month::Feb, 30, 22, 59, 72);
-	ASSERT_EQ(2010, date.getYear());
-	ASSERT_EQ(Month::Mar, date.getMonth());
-	ASSERT_EQ(2, date.getDay());
-	ASSERT_EQ(23, date.getHours());
-	ASSERT_EQ(0, date.getMinutes());
-	ASSERT_EQ(12, date.getSeconds());
-}
-
 
 TEST(AddTimeTests, AddSeconds) {
 	Date date = Date(0, 0, 0);
@@ -178,19 +120,4 @@ TEST(AddTimeTests, UnmodifyingLogic) {
 	ASSERT_EQ(1, date.getHours());
 	ASSERT_EQ(1, date.getMinutes());
 	ASSERT_EQ(1, date.getSeconds());
-}
-
-
-TEST(ToStingTests, AllFields) {
-	Date date(2017, Month::Jan, 1, 22, 22, 33);
-	std::string str = date.toString();
-	ASSERT_EQ("2017-Jan-1 22::22::33", str);
-	date = Date(1, Month::Jul, 30, 1, 1, 1);
-	str = date.toString();
-	ASSERT_EQ("1-Jul-30 1::1::1", str);
-}
-
-int main(int argc, char** argv) {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
 }
---
+++ 02/Tests/ToStringTests.cpp
@@ -0,0 +1,13 @@
+#include "gtest.h"
+#include "../Source/Date.h"
+
+using namespace DateTools;
+
+TEST(ToStingTests, AllFields) {
+	Date date(2017, Month::Jan, 1, 22, 22, 33);
+	std::string str = date.toString();
+	ASSERT_EQ("2017-Jan-1 22::22::33", str);
+	date = Date(1, Month::Jul, 30, 1, 1, 1);
+	str = date.toString();
+	ASSERT_EQ("1-Jul-30 1::1::1", str);
+}
